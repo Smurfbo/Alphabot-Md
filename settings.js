ba21e292ad93846cc0bbad25148efabed46807f6
Smurfbo <109736473+Smurfbo@users.noreply.github.com>
2022-07-22 11:28:17 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -24,8 +24,8 @@ global.language = indonesia  //change indonesia to english if you don't understa
 
 // Other
 // Other
-global.botname = "Alphabot-Mdོ" //namabot kalian
-global.ownername= "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //nama kalian
+global.botname = "Smurf-Bot" //namabot kalian
+global.ownername= "FuckCool᭄ོ ×፝֟͜×" //nama kalian
 global.myweb ="https://api.zeeoneofc.xyz" //bebas asal jan hapus
 global.youtube = "https://youtube.com/c/ZeeoneOfc" //bebas asal jan hapus
 global.github = "https://zeeone-ofc.github.io/" //bebas
@@ -33,14 +33,14 @@ global.email = "zeeoneofc@gmail.com" //bebas
 global.region = "Indonesia" //bebas
 global.timezone = 'Asia/Jakarta' //  timezone wib
 global.premium = ['62887435047326'] //premium user
-global.owner = ["622150996855","6285342106390"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "62887435047326" // nomor wa kalian
-global.ownernomerr = "+62887435047326" //nmr wa kalian
+global.owner = ["6289666430345","6289666430345"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "6289666430345" // nomor wa kalian
+global.ownernomerr = "+6289666430345" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
-global.packname = '© Alphabot-Mdོ' //sticker wm ubah
-global.author = 'Di Buat Oleh ZeeoneOfc' //sticker wm ganti nama kalian
+global.packname = '© Smurf-Bot' //sticker wm ubah
+global.author = 'Di Buat Oleh FuckCool᭄ོ ×፝֟͜×' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.typemenu = 'document'
 
